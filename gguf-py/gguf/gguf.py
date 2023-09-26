fd5db6734e5d76937f1a68d7d50bf5d8f2304f09
Cebtenzzre <cebtenzzre@gmail.com>
2023-09-26 16:23:57 -0400

    gguf : add MPT model architecture

--- gguf-py/gguf/gguf.py
+++ gguf-py/gguf/gguf.py
@@ -202,6 +202,17 @@ MODEL_TENSOR_NAMES: dict[MODEL_ARCH, dict[MODEL_TENSOR, str]] = {
         MODEL_TENSOR.FFN_DOWN:      "blk.{bid}.ffn_down",
         MODEL_TENSOR.FFN_UP:        "blk.{bid}.ffn_up",
     },
+    MODEL_ARCH.MPT: {
+        MODEL_TENSOR.TOKEN_EMBD:    "token_embd",
+        MODEL_TENSOR.OUTPUT_NORM:   "output_norm",
+        MODEL_TENSOR.OUTPUT:        "output",
+        MODEL_TENSOR.ATTN_NORM:     "blk.{bid}.attn_norm",
+        MODEL_TENSOR.ATTN_QKV:      "blk.{bid}.attn_qkv",
+        MODEL_TENSOR.ATTN_OUT:      "blk.{bid}.attn_output",
+        MODEL_TENSOR.FFN_NORM:      "blk.{bid}.ffn_norm",
+        MODEL_TENSOR.FFN_DOWN:      "blk.{bid}.ffn_down",
+        MODEL_TENSOR.FFN_UP:        "blk.{bid}.ffn_up",
+    },
     MODEL_ARCH.GPT2: {
         # TODO
     },
@@ -258,6 +269,7 @@ class TensorNameMap:
             "model.norm",                # llama-hf baichuan
             "norm",                      # llama-pth
             "embeddings.LayerNorm",      # bert
+            "transformer.norm_f",        # mpt
         ),
 
         # Rope frequencies
