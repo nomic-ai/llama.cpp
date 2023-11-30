f494be00b6a03b16c80e9f31a23411549cae3be5
Jared Van Bortel <jared@nomic.ai>
2023-11-30 16:59:08 -0500

    Revert "MPT : clone wte to output at load time"
    It seems like upstream isn't interested in this change for the time
    being [1], and we are going to break compatiblity with Nomic's previous
    conversion of MPT because of changes to the BPE tokenizer [2], so let's
    remove this change to minimize the diff.
    
    This reverts commit 69c505edad1ead6f49823858b3e131e1d57d2fda.
    
    [1] https://github.com/ggerganov/llama.cpp/pull/3626
    [2] https://github.com/ggerganov/llama.cpp/pull/3252

--- convert-hf-to-gguf.py
+++ convert-hf-to-gguf.py
@@ -462,6 +462,11 @@ class MPTModel(Model):
 
             self.gguf_writer.add_tensor(new_name, data)
 
+            # note: MPT output is tied to (same as) wte in original model;
+            # for easier implementation in llama.cpp it's duplicated in GGUF, though :/
+            if new_name == "token_embd.weight":
+                self.gguf_writer.add_tensor("output.weight", data)
+
 
 class BaichuanModel(Model):
     def set_vocab(self):
