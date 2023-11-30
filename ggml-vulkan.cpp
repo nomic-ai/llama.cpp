8b9ffb3b0dc1f2ae979e2a9f9129829fa80d5c36
Jared Van Bortel <jared@nomic.ai>
2023-11-30 16:50:20 -0500

    vulkan : correctly fix use-after-free in ggml_vk_current_device
    The previous attempt actually broke GPU inference with the 'main'
    example, which was previously working.
    
    deviceName is a vk::ArrayWrapper1D. Be careful when we convert it to a
    std::string, so we don't get null bytes at the end.

--- ggml-vulkan.cpp
+++ ggml-vulkan.cpp
@@ -290,8 +290,9 @@ ggml_vk_device ggml_vk_current_device() {
         return ggml_vk_device();
 
     std::vector<ggml_vk_device> devices = ggml_vk_available_devices(0);
-    ggml_vk_filterByName(devices, komputeManager()->physicalDevice()->getProperties().deviceName);
-    return devices.empty() ? ggml_vk_device() : devices.front();
+    ggml_vk_filterByName(devices, komputeManager()->physicalDevice()->getProperties().deviceName.data());
+    GGML_ASSERT(!devices.empty());
+    return devices.front();
 }
 
 ggml_kompute_context *ggml_vk_init() {
