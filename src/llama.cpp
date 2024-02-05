e8bec6f30e7f2e4ea46f4de6ba8328503de252bc
Jared Van Bortel <jared@nomic.ai>
2024-02-05 16:02:01 -0500

    kompute : disable GPU offload for Mixtral
    We haven't implemented the necessary GPU kernels yet.
    
    Fixes this crash:
    
    ggml_vk_graph_compute: error: unsupported op 'ARGSORT'
    GGML_ASSERT: /home/jared/src/forks/gpt4all/gpt4all-backend/llama.cpp-mainline/ggml-kompute.cpp:1508: !"unsupported op"

--- src/llama.cpp
+++ src/llama.cpp
@@ -8912,6 +8912,7 @@ static int llama_model_load(const std::string & fname, llama_model & model, llam
 #ifdef GGML_USE_KOMPUTE
         if (params.n_gpu_layers > 0 && (
             !(model.arch == LLM_ARCH_LLAMA || model.arch == LLM_ARCH_FALCON)
+            || model.hparams.n_expert > 0
             || !(
                 model.ftype == LLAMA_FTYPE_ALL_F32 ||
                 model.ftype == LLAMA_FTYPE_MOSTLY_F16 ||
